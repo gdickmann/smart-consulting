267b3b2364cd930bac41c98dc11e18dddb900ad8
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2022-05-19 21:31:27 -0300

    tá tudo funcionando só falta refatorar

--- Main.java
+++ Main.java
@@ -11,24 +11,7 @@ import utils.PhoneValidator;
 public class Main {
 	
 	public static void main(String[] args) {
-		String[] number = {"+44 65465444", "+56 918 878 443", "+82 918 878 443", "+666 65465444", "+44 65465444", "3333"};
-		List<String> allPhoneNumbers = new ArrayList<>();
-
-		for (String phoneNumber : number) {
-			/** If a phone number is short, it's automatically a portuguese number */
-			if (PhoneValidator.isValidShortNumber(phoneNumber)) {				
-				allPhoneNumbers.add("Portuguese");
-			}
-			if (PhoneValidator.isValidLongNumber(phoneNumber)) {
-				PhoneValidator.insertLongNumber(phoneNumber, allPhoneNumbers);
-			}
-		}
-
-		HashSet<String> uniqueValues = new HashSet<>(allPhoneNumbers);
-		for (String value : uniqueValues) {
-			System.out.println(value + " " + Collections.frequency(allPhoneNumbers, value));
-		}
-
+		readPhoneNumbers(args);
     }
 
 	private static void readPhoneNumbers(String[] file) {
@@ -38,9 +21,24 @@ public class Main {
 			reader = new BufferedReader(new FileReader(file[0]));
 			String line = reader.readLine();
 
+			List<String> allCountryNumbers = new ArrayList<>();
+
 			while (line != null) {
-				System.out.println(line);
+				/** If a phone number is short, it's automatically a portuguese number */
+				if (PhoneValidator.isValidShortNumber(line)) {				
+					allCountryNumbers.add("Portuguese");
+				}
+				if (PhoneValidator.isValidLongNumber(line)) {
+					PhoneValidator.insertLongNumber(line, allCountryNumbers);
+				}
+
 				line = reader.readLine();
+			}
+
+			
+			HashSet<String> uniqueValues = new HashSet<>(allCountryNumbers);
+			for (String value : uniqueValues) {
+				System.out.println(value + " " + Collections.frequency(allCountryNumbers, value));
 			}
 			
 			reader.close();
